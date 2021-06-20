57bb690b671a29317c27ab27f9a75366ba9018e7
Peter Karnaukh <peterkarnaukh@gmail.com>
2021-06-20 11:38:57 +0300

    tabs setion inited

--- !sukhanovo/js/script.js
+++ !sukhanovo/js/script.js
@@ -54,5 +54,127 @@ testWebP(function (support) {
     var myYbox = new yBox();
     myYbox.init();
   };
+
+  //accordion
+
+  const accordions = document.querySelectorAll('.accordion'),
+    allContents = document.querySelectorAll('.accordion__content'),
+    mapSvg = document.querySelector('.map__svg');
+
+
+  accordions.forEach(accordion => {
+    accordion.addEventListener('click', (e) => {
+      const self = e.currentTarget;
+      const control = self.querySelector('.accordion__control');
+      const content = self.querySelector('.accordion__content');
+
+      if (!self.classList.contains('open')) {
+        control.setAttribute('aria-expanded', true);
+        content.setAttribute('aria-hidden', false);
+        document.querySelectorAll('.accordion.open').forEach(element => {
+          element.classList.remove('open')
+        });
+        self.classList.add('open');
+        allContents.forEach(element => {
+          element.style.maxHeight = null;
+        });
+        content.style.maxHeight = content.scrollHeight + 'px';
+
+        //icons activity
+
+       if (mapSvg) {
+         const mapIcons = document.querySelectorAll('.map__icon');
+         const currentCat = self.dataset.cat;
+         
+         mapIcons.forEach(icon => {
+          if (icon.classList.contains('active')) {
+            icon.classList.remove('active');
+          }
+         });
+         
+         mapSvg.querySelectorAll('[data-filter =' + currentCat + ']').forEach(element => {
+          element.classList.add('active');
+        });
+       }
+
+      } else {
+        control.setAttribute('aria-expanded', false);
+        content.setAttribute('aria-hidden', true);
+        self.classList.remove('open');
+        content.style.maxHeight = null;
+
+        //icons activity
+
+        if (mapSvg) {
+          const mapIcons = document.querySelectorAll('.map__icon');
+          mapIcons.forEach(icon => {
+            if (icon.classList.contains('active')) {
+              icon.classList.remove('active');
+            }
+          });
+        }
+      }
+    });
+  });
+
+  // tabs
+
+  const tabs = document.querySelector('.tabs');
+  const tabsBtn = document.querySelectorAll('.tabs__btn');
+  const tabsContent = document.querySelectorAll('.tabs__content');
+
+  if (tabs) {
+    tabs.addEventListener('click', (e) => {
+      if (e.target.classList.contains('tabs__btn')) {
+        const tabsPath = e.target.dataset.tabsPath;
+        tabsBtn.forEach(el => {
+          el.classList.remove('tabs__btn_active')
+        });
+        document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('tabs__btn_active');
+        tabsHandler(tabsPath);
+      }
+
+      if (e.target.classList.contains('tabs__arrow--prev')) {
+        let activeBtn = document.querySelector('.tabs__btn_active');
+        let activeParent = activeBtn.closest('.tabs__item');
+        let previousParent = activeParent.previousElementSibling;
+
+        if (previousParent) {
+          let prevActive = previousParent.querySelector('.tabs__btn')
+          tabsBtn.forEach(el => {
+            el.classList.remove('tabs__btn_active')
+          });
+          prevActive.classList.add('tabs__btn_active');
+
+          let path = prevActive.dataset.tabsPath;
+          tabsHandler(path);
+        }
+      }
+
+      if (e.target.classList.contains('tabs__arrow--next')) {
+        let activeBtn = document.querySelector('.tabs__btn_active');
+        let activeParent = activeBtn.closest('.tabs__item');
+        let nextParent = activeParent.nextElementSibling;
+
+        if (nextParent) {
+          let nextActive = nextParent.querySelector('.tabs__btn');
+          tabsBtn.forEach(el => {
+            el.classList.remove('tabs__btn_active')
+          });
+          nextActive.classList.add('tabs__btn_active');
+
+          let path = nextActive.dataset.tabsPath;
+          tabsHandler(path);
+        }
+      }
+    });
+  }
+
+  const tabsHandler = (path) => {
+    tabsContent.forEach(el => {
+      el.classList.remove('tabs__content_active')
+    });
+    document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content_active');
+  };
 });
 
